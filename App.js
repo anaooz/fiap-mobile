98022b32c7fe2a98696dc6b0635b1f649fd77ae8
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-15 09:04:17 -0300

    teste async

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
+import { FlatList } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [movies, setMovies] = useState([])
+
+  console.log(movies)
+
+  const getMovies = () => {
+    try {
+      const response = fetch('https://reactnative.dev/movies.json').then(
+        (response) => response.json
+      ).then(
+        (response) => console.log(response)
+      )
+      console.log(json)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const getMoviesAsync = async () => {
+    try {
+      const response = await fetch('https://reactnative.dev/movies.json')
+      
+      const json = await response.json
+      
+      setMovies(json.movies)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getMovies()
+  }, [])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+      data={movies}
+      renderItem={({ item: movie }) => {
+        return(
+          <Text>{movie.title}</Text>
+        )
+      }}/>
     </View>
   );
 }
